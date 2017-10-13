3872ad0b1f02143d19d477af30df94ac1e848573
Caleb Sharp <epicsharp11@gmail.com>
2017-10-13 16:24:03 -0700

    Make it more modular

--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
-const TelegramBot = require('node-telegram-bot-api');
 const natural = require('natural');
-const sentiment = require('sentiment');
-const util = require('util');
-
-const config = require('./config');
 
 const intents = require('./intents.json').intents;
 
 const handlers = {
   reminder(text, response) {
-    console.log(text);
-
     return response.replace('{reminder.text}', `'${text}'`);
   },
   random(text, response) {
@@ -30,12 +23,12 @@ const learnIntents = (intents) => {
 
   classifier.train();
   return classifier;
-}
+};
 
-const generateResponse = (intents, classifier, text) => {
+const classifier = learnIntents(intents);
+
+const generateResponse = (text) => {
   const tag = classifier.classify(text);
-  const c = classifier.getClassifications(text);
-  const sen = sentiment(text);
 
   const intent = intents[tag];
   
@@ -45,35 +38,8 @@ const generateResponse = (intents, classifier, text) => {
     return handlers[tag].call(null, text, response);  
   }
   return response;
-}
+};
 
-
-// const bot = new TelegramBot(config.token, {
-//   // webHook: {
-//   //   host: config.host,
-//   //   port: config.port,
-//   // }
-//   polling: true
-// });
-
-// bot.setWebHook(config.ngrokHost);
-  
-const classifier = learnIntents(intents);
-
-// bot.on('message', (msg) => {
-//   const response = generateResponse(intents, classifier, msg.text);
-//   const chatId = msg.chat.id;
-
-//   bot.sendMessage(chatId, `${response}`);
-// });
-
-console.log('bot working');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-process.stdin.on('data', text => {
-  input = util.inspect(text);
-  const response = generateResponse(intents, classifier, input);
-
-  console.log(response);
-});
+module.exports = { 
+  generateResponse,
+};
