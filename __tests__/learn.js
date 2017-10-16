ba04bffed8137e99c9640ad84e0accb4346b6c9e
Caleb Sharp <epicsharp11@gmail.com>
2017-10-16 07:34:35 -0700

    Add tests

---
+++ __tests__/learn.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+const { learn, getClassifier, } = require('../src/learn');
+const intentsFile = require('../src/intents.json').intents;
+
+const natural = require('natural');
+
+const intents = {
+  hello: {
+    tag: 'hello',
+    patterns: [
+      'hello!',
+    ],
+    responses: [
+      'hi!',
+    ],
+  },
+};
+
+const classifierFile = __dirname + '/classifier.json';
+
+test('learn: creates a classifier based on an intents object', () => {
+  const classifier = learn(intents, classifierFile);
+
+  expect(classifier).toBeInstanceOf(natural.BayesClassifier);
+});
+
+test('learn: it saves the classifier into a json file after learning', () => {
+  natural.BayesClassifier.load(classifierFile, null, (err, classifier) => {
+    expect(classifier).toBeInstanceOf(natural.BayesClassifier);
+    expect(err).toEqual(null);
+  });
+
+  expect(true);
+});
+
+test('getClassifier: loads classifier from a file if it exists', () => {
+  getClassifier(classifierFile)
+    .then(c => expect(c).toBeInstanceOf(natural.BayesClassifier));
+});
+
+test('getClassifier: creates a new classifier from intents.json file if file doesn\'t exists', () => {
+  getClassifier(__dirname + '/notafile.json')
+    .then(c => {
+      expect(c).toEqual(learn(intentsFile));
+    });
+});
+
+afterAll(() => {
+  fs.unlinkSync(classifierFile);
+});
